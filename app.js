7399bfaed5ee98592d03f25c75f5a352ed42f9d0
Joonas Vanhalakka <joonas.vanhalakka@helsinki.fi>
2021-11-14 15:17:13 +0200

    testing failed health check

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-//const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-/*
+
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line
 })
-*/
+
 app.get('/health', (req, res) => {
   res.send('ok')
 })
